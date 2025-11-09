dec1676595597b95323f297acf9f7ca3c26e6fd5
sams <133288211+sahyam2023@users.noreply.github.com>
2025-11-09 22:11:24 +0530

    fixes still buggy

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
 from .user import User
 from .exam import Exam, Question, Submission
-from .proctor import UserFace, ProctoringLog, ProctoringSummary
+from .proctor import UserFace, ProctorLog
 from .assignment import ExamAssignment
